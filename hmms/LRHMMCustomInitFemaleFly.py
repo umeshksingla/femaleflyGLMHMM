59381cd47185fbb7e2934fde991274925765c131
Umesh Singla <umeshksingla@gmail.com>
2025-02-18 16:37:25 -0500

    support LRHMM model with weights from simple LR

---
+++ hmms/LRHMMCustomInitFemaleFly.py
@@ -0,0 +1,36 @@
+import jax
+import numpy as np
+import jax.random as jr
+from hmms.LRHMMFemaleFly import LRHMMFemaleFly
+from hmms.LRFemaleFly import LRFemaleFly
+
+from utilities import fitting
+
+jax.config.update("jax_enable_x64", True)
+
+
+class LRHMMCustomInitFemaleFly(LRHMMFemaleFly):
+
+    prefix = 'lrhmmci'
+
+    def fit(self, emissions, inputs):
+        key = jr.PRNGKey(self.seed)
+        
+        lr = LRFemaleFly(self.data_config, self.model_config)
+        lr.fit(emissions, inputs)
+        print("LR initial params computed (done):", lr.learned_params)
+        
+        W = np.repeat(lr.learned_params['w'], repeats=self.num_states, axis=0)
+        b = np.repeat(lr.learned_params['b'], repeats=self.num_states, axis=0)
+        W = W + np.random.random(W.shape) * 0.01
+        b = b + np.random.random(b.shape) * 0.01
+        print("W", W.shape, "b", b.shape)
+        print(b)
+        em_params, em_lps = fitting.fitEMCustomInit(key, self.model, emissions, train_inputs=inputs, 
+                                                    emission_weights=W, emission_biases=b)
+        self.learned_params = em_params
+        self.learned_lps = em_lps
+        self.update_status()
+        return
+
+# python run_single.py --mc '{"names": "lrhmmci", "seeds": 6205, "num_states": 2, "transition_matrix_stickiness": 10}'
--- utilities/fitting.py
+++ utilities/fitting.py
@@ -1,9 +1,36 @@
 import time
 
 import jax.random as jr
+import jax.numpy as jnp
 import optax
 
-from plotting.plots import print_params
+
+def fitEMCustomInit(key, hmm, train_emissions, train_inputs,
+                    initial_probs=None,
+                    transition_matrix=None,
+                    emission_weights=None,
+                    emission_biases=None,
+                    emission_covariances=None):
+    print("=== START emCustomInit ==========")
+    s = time.time()
+    _, key = jr.split(key)
+    em_params, em_props = hmm.initialize(key, initial_probs=initial_probs,
+                                         transition_matrix=transition_matrix,
+                                         emission_weights=emission_weights,
+                                         emission_biases=emission_biases,
+                                         emission_covariances=emission_covariances,
+                                         )
+    em_params, em_losses = hmm.fit_em(em_params,
+                                        em_props,
+                                        train_emissions,
+                                        train_inputs,
+                                        num_iters=200, verbose=True)
+    em_lps = -em_losses / train_emissions.size
+    print("em_lps:", em_lps)
+    e = time.time()
+    print("=== END emCustomInit ==========")
+    print(f"Time taken to fit EM: {round(e - s, 2)}s")
+    return em_params, em_lps
 
 
 def fitEM(key, hmm, train_emissions, train_inputs):
@@ -17,34 +44,61 @@ def fitEM(key, hmm, train_emissions, train_inputs):
                                         train_inputs,
                                         num_iters=200, verbose=True)
     em_lps = -em_losses / train_emissions.size
+    print("em_lps:", em_lps)
     e = time.time()
     print("=== END em ==========")
     print(f"Time taken to fit EM: {round(e - s, 2)}s")
     return em_params, em_lps
 
 
-# fbgd_params, fbgd_lps = fitting.fitSGD(key, lrhmm, train_emissions, train_inputs)
-# learned_params = fbgd_params
-# learned_lps = fbgd_lps
-def fitSGD(key, lrhmm, train_emissions, train_inputs):
+def fitSGDCustomInit(key, hmm, train_emissions, train_inputs,
+                    initial_probs=None,
+                    transition_matrix=None,
+                    emission_weights=None,
+                    emission_biases=None,
+                    emission_covariances=None):
+    print("=== START sgdCustomInit ==========")
+    s = time.time()
+    fbgd_key, key = jr.split(key)
+    fbgd_params, fbgd_props = hmm.initialize(key, initial_probs=initial_probs,
+                                         transition_matrix=transition_matrix,
+                                         emission_weights=emission_weights,
+                                         emission_biases=emission_biases,
+                                         emission_covariances=emission_covariances,
+                                         )
+    fbgd_params, fbgd_losses = hmm.fit_sgd(fbgd_params,
+                                             fbgd_props,
+                                             jnp.asarray(train_emissions),
+                                             jnp.asarray(train_inputs),
+                                             optimizer=optax.sgd(learning_rate=1e-2, momentum=0.95),
+                                             batch_size=5,
+                                             num_epochs=100,
+                                             key=fbgd_key)
+    # fbgd_lps = -fbgd_losses / train_emissions.size    # NO NEED?
+    fbgd_lps = fbgd_losses
+    e = time.time()
+    print(f"Time taken to fit SGD: {round(e - s, 2)}s")
+    print("=== END sgdCustomInit ==========")
+    return fbgd_params, fbgd_lps
+
+
+def fitSGD(key, hmm, train_emissions, train_inputs):
     print("=== START sgd ==========")
     s = time.time()
     fbgd_key, key = jr.split(key)
-    fbgd_params, fbgd_props = lrhmm.initialize(key)
-    fbgd_params, fbgd_losses = lrhmm.fit_sgd(fbgd_params,
+    fbgd_params, fbgd_props = hmm.initialize(key)
+    fbgd_params, fbgd_losses = hmm.fit_sgd(fbgd_params,
                                              fbgd_props,
                                              train_emissions,
                                              train_inputs,
                                              optimizer=optax.sgd(learning_rate=1e-2, momentum=0.95),
-                                             batch_size=1,
+                                             batch_size=5,
                                              num_epochs=400,
                                              key=fbgd_key)
     # fbgd_lps = -fbgd_losses / train_emissions.size    # NO NEED?
     fbgd_lps = fbgd_losses
     e = time.time()
     print(f"Time taken to fit SGD: {round(e - s, 2)}s")
-    print("== FITTED (SGD) ===========")
-    print_params(fbgd_params)
     print("=== END sgd ==========")
     return fbgd_params, fbgd_lps
 
